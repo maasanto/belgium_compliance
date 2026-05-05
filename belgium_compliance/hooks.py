89f15bd221df606dba7f3ec138d7af3f39325ad1
Antoine Maas <antoine.maas@okte.io>
2026-05-05 12:55:51 +0200

    feat: add Frappe app skeleton with ERPNext as required app

---
+++ belgium_compliance/hooks.py
@@ -0,0 +1,23 @@
+app_name = "belgium_compliance"
+app_title = "Belgium Compliance"
+app_publisher = "Antoine Maas"
+app_description = (
+	"Belgian regional compliance for ERPNext (PCMN, VAT declaration, INTERVAT, intracommunity reporting)"
+)
+app_email = "antoine.maas@gmail.com"
+app_license = "gpl-3.0"
+
+# Required apps
+# ------------------
+required_apps = ["erpnext"]
+
+# Installation
+# ------------
+# after_install = "belgium_compliance.install.after_install"
+
+# Fixtures
+# --------
+fixtures = [
+	{"dt": "Belgian VAT Grid"},
+	{"dt": "Belgian VAT Tax Definition"},
+]
